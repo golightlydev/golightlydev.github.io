294dc5eaa4f6674e79d4909f3f458100681ddb3b
golightly <18671493+golightly@users.noreply.github.com>
2019-04-19 11:27:15 -0400

    removed old navigation, added new

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-const left = () => {
-    if(currentElement === 0)
-        return;
-    if(currentElement === 1)
-        container.style.transform = "translateX(0%)";
-    else if(currentElement === 2)
-        container.style.transform = "translateX(-25%)";
-    else if(currentElement === 3)
-        container.style.transform = "translateX(-50%)";
-    --currentElement;
+const home = () => {
+    container.style.transform = "translateX(0%)";
 }
 
-const right = () => {
-    if(currentElement === 3)
-        return;
-    if(currentElement === 0)
-        container.style.transform = "translateX(-25%)";
-    else if(currentElement === 1)
-        container.style.transform = "translateX(-50%)";
-    else if(currentElement === 2)
-        container.style.transform = "translateX(-75%)";
-    ++currentElement;
+const updates = () => {
+    container.style.transform = "translateX(-25%)";
+}
+
+const portfolio = () => {
+    container.style.transform = "translateX(-50%)";
+}
+
+const about = () => {
+    container.style.transform = "translateX(-75%)";
 }
 
 var currentElement = 0;
 const container = document.getElementById("pageContainer");
 container.style.transition = "transform 1s";
-const buttonLeft = document.getElementById("left");
-buttonLeft.addEventListener("click", left);
-const buttonRight = document.getElementById("right");
-buttonRight.addEventListener("click", right);
+const homeButton = document.getElementById("homeButton");
+homeButton.addEventListener("click", home);
+const updatesButton = document.getElementById("updatesButton");
+updatesButton.addEventListener("click", updates);
+const portfolioButton = document.getElementById("portfolioButton");
+portfolioButton.addEventListener("click", portfolio);
+const aboutButton = document.getElementById("aboutButton");
+aboutButton.addEventListener("click", about);
