467926964692bf57b6ffa53094b856a4334f8c27
golightly <18671493+golightly@users.noreply.github.com>
2019-06-23 16:33:05 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function formSubmit(event) {
+    name = content.children[0].children[0].children[1].value;
+    for(let a = 0; a < 3; ++a) {
+        if(content.children[0].children[0].children[5].children[a].children[0].checked)
+            portrait = a;
+    }
+    if(name !== null && portrait !== null) {
+        content.children[0].children[0].children[8].removeEventListener("click", formSubmit);
+        console.log("this triggers");
+    }
+}
+
+const content = document.getElementById("content");
+content.children[0].children[0].children[8].addEventListener("click", formSubmit);
+var name = null;
+var portrait = null;
