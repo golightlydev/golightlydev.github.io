39ec9cd4a42205aeba3fa4b8a13b60d2142cf4f0
golightly <18671493+golightly@users.noreply.github.com>
2019-06-30 13:14:32 -0400

    Update turnBaseScript.js

--- turnBaseScript.js
+++ turnBaseScript.js
@@ -1,3 +1,11 @@
+getLine = (text, array, index) => {
+    for(let a = 0; index < text.length && text[index] != '\n'; ++index) {
+        array[a] = text[index];
+        ++a;
+    }
+    return ++index;
+}
+
 loadFile = () => {
     let loadTxtHandle = document.getElementById("loadTxt");
     let text = loadTxtHandle.contentWindow.document.body.childNodes[0].innerHTML;
@@ -5,15 +13,8 @@ loadFile = () => {
     let index = 0;
     let testString1 = [];
     let testString2 = [];
-    for(let a = 0; index < text.length && text[index] != '\n'; ++index) {
-        testString1[a] = text[index];
-        ++a;
-    }
-    ++index;
-    for(let a = 0; index < text.length && text[index] != '\n'; ++index) {
-        testString2[a] = text[index];
-        ++a;
-    }
+    index = getLine(text, testString1, index);
+    index = getLine(text, testString2, index);
     console.log(testString1);
     console.log(testString2);
 }
