cac0edf2bc0ae4b7165e6d9ab39734954a0d3b82
golightly <18671493+golightly@users.noreply.github.com>
2019-06-30 13:00:51 -0400

    testing line reading

--- turnBaseScript.js
+++ turnBaseScript.js
@@ -1,5 +1,19 @@
 loadFile = () => {
-    var loadTxtHandle = document.getElementById("loadTxt");
-    var text = loadTxtHandle.contentWindow.document.body.childNodes[0].innerHTML;
+    let loadTxtHandle = document.getElementById("loadTxt");
+    let text = loadTxtHandle.contentWindow.document.body.childNodes[0].innerHTML;
     console.log(text);
+    let index = 0;
+    let testString1 = [];
+    let testString2 = [];
+    for(let a = 0; index < text.length || text[index] == '\n'; ++index) {
+        testString1[a] = text[index];
+        ++a;
+    }
+    ++index;
+    for(let a = 0; index < text.length || text[index] == '\n'; ++index) {
+        testString2[a] = text[index];
+        ++a;
+    }
+    console(testString1);
+    console(testString2);
 }
