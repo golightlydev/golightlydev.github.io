a08d077caa31ea51dd24f74c9243b9c03241d27e
golightly <18671493+golightly@users.noreply.github.com>
2019-07-16 20:18:00 -0400

    working on nodeServer

--- autoCrawlerHighScoresScript.js
+++ autoCrawlerHighScoresScript.js
@@ -1,13 +1,24 @@
-var test = "test for swears: fuck you asshole!";
+class Data {
+    constructor(name, number) {
+        this.name = name;
+        this.number = number;
+    }
+    getName() { return this.name; }
+    setName(name) { this.name = name; }
+    getNumber() { return this.number; }
+    setNumber(number) { this.number = number; }
+};
+
+var data = new Data("nick4", 3);
 
 fetch('http://localhost:3000/autoCrawlerHighScores').then(response => response.json())
     .then(data => {
         console.log(data);
     });
     
-fetch('http://localhost:3000/', {
+fetch('http://localhost:3000/autoCrawlerHighScores', {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({test}),
+    body: JSON.stringify({data}),
 }).then(response => response.json())
     .then(data => {console.log(data);});
