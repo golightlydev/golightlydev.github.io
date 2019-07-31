b2cb57400d8e86dd608e8c2b86f20084abb007b4
golightly <18671493+golightly@users.noreply.github.com>
2019-07-30 20:46:46 -0400

    updated nodeServer

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -6,40 +6,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 const fs = require('fs');
-const readline = require('readline');
 const Filter = require('bad-words');
 var filter = new Filter();
 
-
-//naive implementation
-
-/*var write = null;
-var writing = false;
-
-function testFunction(num) {
-    write.write(num);
-}
-
-write = fs.createWriteStream("data.txt", {flags:'a'});
-writing
-for(let a = 0; a < 3; ++a) {
-    testFunction(a.toString());
-}
-write.end();
-write = null;
-write = fs.createWriteStream("data.txt", {flags:'a'});
-for(let a = 0; a < 5; ++a) {
-    testFunction(a.toString());
-}
-write.end();
-write = null;*/
-
 var fileGate = false; //gates access to file to one user at a time to prevent data loss
+var postErrorCheck = false;
+var getErrorCheck = false;
 
-if(!fs.existsSync("data.txt"))
-    fs.writeFile("data.txt", "0", error => {
+if(!fs.existsSync("data.txt")) {
+    fs.writeFile("data.txt", "0\n1\n2", error => {
         console.log(error);
     });
+}
 
 class Data {
     constructor(name, number) {
@@ -57,75 +35,60 @@ var data = {
     length: 0,
 };
 
-app.post('/autoCrawlerHighScores', (request, response) => {
-    if(fileGate)
-        window.setTimeout(fileGate, 10);
+var dataMemberIterator = 'name';
+var dataNameTemp = null;
+
+var executionNum = 0;
+
+function mainGet() {
+
+}
+
+function mainPost() {
+    console.log("fileGate: " + fileGate);
+    console.log("executionNum: " + executionNum);
+    executionNum = 0;
     fileGate = true;
-    /*let readInterface = readline.createInterface( {
-        input: fs.createReadStream('data.txt'),
-        output: process.stdout,
-        console: false,
-    });
-    data.length = 0;
+    data.length = null;
     data.array = [];
-    readInterface.on('line', function(line) {
-        data.length = Number(line);
-    });
-    for(let a = 0; a < data.length; ++a) {
-        let name = null;
-        let number = 0;
-        readInterface.on('line', function(line) {
-            name = line;
-        });
-        readInterface.on('line', function(line) {
-            number = Number(line);
-        });
-        data.array.push(new Data(name, number));
-    }
-    readInterface.close();*/
-    if(data.length === 0) {
-        ++data.length;
-        data.array.push(new Data(request.body.data.name, request.body.data.number));
-    }
-    else {
-        for(let a = 0; a < data.length; ++a) {
-            if(Number(request.body.data.number) > data.array[a].number) {
-                let temp = new Data(data.array[a].name, data.array[a].number);
-                data.array[a].name = filter.clean(request.body.data.name);
-                if(!isNaN(request.body.data.number))
-                    data.array[a].number = request.body.data.number;
-                ++data.length;
-                data.array.push(new Data(null, null));
-                for(let b = a + 1; b < data.length; b+=2) {
-                    let temp2 = new Data(data.array[b].name, data.array[b].number);
-                    data.array[b].name = temp.name;
-                    data.array[b].number = temp.number;
-                    if(b+1 < data.length) {
-                        temp.name = data.array[b+1].getName();
-                        temp.number = data.array[b+1].number;
-                        data.array[b+1].name = temp2.name;
-                        data.array[b+1].number = temp2.number;
-                    }
-                }
-                if(data.length > 10) {
-                    data.array.pop();
-                    --data.length;
-                }
-                break;
-            }
-            if(a+1 === data.length) {
-                ++data.length;
-                data.array.push(new Data(request.body.data.name, request.body.data.number));
-                break;
-            }
+    postErrorCheck = false;
+    fs.readFile('data.txt', (error, readData) => {
+        if(error) {
+            console.log(error);
+            postErrorCheck = true;
         }
+        else {
+            let fileContent = readData.toString().split('\n');
+            console.log(fileContent);
+        }
+    });
+    if(postErrorCheck) {
+        response.json("0");
+        return 0;
     }
-    console.log(data);
-    fileGate = false;
-    response.json({body: 'post succeeded'});
+}
+
+function gate(executionType) {
+    if(!fileGate) {
+        if(executionType === "post")
+            mainPost();
+        else if(executionType === "get")
+            mainGet();
+    }
+    else if(fileGate) {
+        ++executionNum;
+        if(executionNum === 200)
+            fileGate = false;
+        setTimeout(gate, 10, executionType);
+    }
+}
+
+app.post('/autoCrawlerHighScores', (request, response) => {
+    gate("post");
 });
 
 app.get('/autoCrawlerHighScores', (request, response) => {
+    //get data from file
     response.json({data});
 });
 
