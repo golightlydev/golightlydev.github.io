45d1ee2b6dca16ade02e083fc9248f5f4de553b5
golightly <18671493+golightly@users.noreply.github.com>
2019-08-01 12:55:10 -0400

    highscore dom first draft

--- autoCrawlerHighScoresScript.js
+++ autoCrawlerHighScoresScript.js
@@ -9,10 +9,36 @@ class Data {
     setNumber(number) { this.number = number; }
 };
 
-var data = new Data("nick7", "120");
+class HighScoreBoard {
+    constructor() {
+        this.data = {
+            array: [new Data(document.getElementById("oneName"), document.getElementById("oneScore")), 
+                new Data(document.getElementById("twoName"), document.getElementById("twoScore")), 
+                new Data(document.getElementById("threeName"), document.getElementById("threeScore")), 
+                new Data(document.getElementById("fourName"), document.getElementById("fourScore")), 
+                new Data(document.getElementById("fiveName"), document.getElementById("fiveScore")), 
+                new Data(document.getElementById("sixName"), document.getElementById("sixScore")), 
+                new Data(document.getElementById("sevenName"), document.getElementById("sevenScore")), 
+                new Data(document.getElementById("eightName"), document.getElementById("eightScore")), 
+                new Data(document.getElementById("nineName"), document.getElementById("nineScore")), 
+                new Data(document.getElementById("tenName"), document.getElementById("tenScore")), ],
+            length: 10,
+        };
+    }
+    populateElement(name, number, index) {
+        this.data.array[index].name.innerText = name;
+        this.data.array[index].number.innerText = number;
+    }
+};
+
+var highScoreBoard = null;
 
 fetch('http://localhost:3000/autoCrawlerHighScores').then(response => response.json())
     .then(data => {
         console.log("get: ");
-        console.log(data);
+        console.log(data.data);
+        highScoreBoard = new HighScoreBoard();
+        for(let a = 0; a < data.data.length; ++a) {
+            highScoreBoard.populateElement(data.data.array[a].name, data.data.array[a].number, a);
+        }
     });
