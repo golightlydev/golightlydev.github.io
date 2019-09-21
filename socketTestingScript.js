b6da26ea5c2543aa1fa62cf9417a1772cddaac40
golightly <golightlydev@gmail.com>
2019-09-21 17:42:03 -0400

    client confirm code

--- socketTestingScript.js
+++ socketTestingScript.js
@@ -1,3 +1,7 @@
 console.log("hello world");
 
 const socket = io('https://damp-brook-48872.herokuapp.com', {path: '/socketTest'});
+
+socket.on('connect', (sock) => {
+    console.log("socket connected");
+});
