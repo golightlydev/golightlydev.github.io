ac20130b07bee61eda4a2c2473d3486ad47f5481
golightly <golightlydev@gmail.com>
2019-09-22 10:55:47 -0400

    testing socket emit

--- socketTestingScript.js
+++ socketTestingScript.js
@@ -4,4 +4,18 @@ const socket = io('https://damp-brook-48872.herokuapp.com', {path: '/socketTest'
 
 socket.on('connect', (sock) => {
     console.log("socket connected");
+
+    socket.on('disconnect', () => {
+        console.log("disconnected");
+        socket = null;
+    });
 });
+
+var connectButton = document.getElementById("connectButton");
+var disconnectButton = document.getElementById("disconnectButton");
+disconnectButton.addEventListener('click', () => {
+    if(socket) {
+        socket.emit('disconnectMessage', {msg1: 'hello message', msg2: 'good bye message'});
+    }
+});
+
